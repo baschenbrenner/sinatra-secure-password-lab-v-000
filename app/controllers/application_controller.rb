142398d6ae824a2921a2dd66988ae8794e986231
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-11-18 16:28:44 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,8 +25,7 @@ class ApplicationController < Sinatra::Base
       redirect "/failure"
     else
         if params[:username] != "" && params[:password] != ""
-
-          @user = User.create(username: params[:username], password: params[:password])
+          User.create(username: params[:username], password: params[:password])
           redirect "/login"
       else
         redirect "/failure"
